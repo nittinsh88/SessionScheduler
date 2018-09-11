3dd3663faabdb36cfba4786b5b6c9c68763ee0dd
nittinsh88 <nittinsh88@gmail.com>
2018-09-11 15:36:06 +0530

    cleaning classes

--- src/com/test/scheduler/reader/InputReader.java
+++ src/com/test/scheduler/reader/InputReader.java
@@ -6,14 +6,22 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+/**
+ * @author nittinsharma
+ * 
+ * Read the file name and take the file path from user
+ *
+ */
 public class InputReader {
 	
 	
 	public static List<String> read(){
 		List<String>  rawSessions=new ArrayList<String>();
-		//System.out.println("Please enter file location");
-		//Scanner scanner=new Scanner(System.in);
+		System.out.println("Please enter file location");
+		Scanner input=new Scanner(System.in);
 		File file = new File("D:\\Hiddentasks\\input.txt");
+		file=new File(input.next());
+		input.close();
 		Scanner scanner = null;
 		try {
 			scanner = new Scanner(file);
@@ -23,7 +31,7 @@ public class InputReader {
 		}
 		} catch (FileNotFoundException e) {
 			
-			e.printStackTrace();
+			System.out.println("error reading file "+e.getMessage());
 		}finally{
 		
 		scanner.close();}
